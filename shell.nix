036c5e0bb64e8b87a4cc71e9b36fe6ece41629c2
Arqam Zia <arqam.mzia@gmail.com>
2024-10-19 02:24:21 +0500

    Install d2

--- shell.nix
+++ shell.nix
@@ -3,11 +3,14 @@
 pkgs.mkShell {
   buildInputs = with pkgs; [
     texliveFull  # TeX Live for LaTeX documents
+    d2 # For the diagrams
+    go # For installing the latest d2 from source
     # Add any additional dependencies here
   ];
 
   shellHook = ''
-    echo "Welcome to your project development shell!"
+    echo "Welcome to the Hawk-tu development shell!"
     echo "TeX Live is available."
+    echo "D2 Diagramming is available."
   '';
 }
