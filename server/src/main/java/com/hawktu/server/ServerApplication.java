ccad0ab4052a22c2b339f9043b279782903d521c
Arqam Zia <arqam.mzia@gmail.com>
2024-10-18 18:58:42 +0500

    Setup axios, start login integration

--- server/src/main/java/com/hawktu/server/ServerApplication.java
+++ server/src/main/java/com/hawktu/server/ServerApplication.java
@@ -1,13 +1,27 @@
 package com.hawktu.server;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        // Load environment variables from .env file
+        DotenvLoader.load();  
 
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    // CommandLineRunner for testing environment variable loading
+    @Bean
+    public CommandLineRunner run() {
+        return args -> {
+            // Print loaded environment variables
+            System.out.println("Keystore Password: " + System.getenv("KEYSTORE_PASSWORD"));
+            System.out.println("Keystore Alias: " + System.getenv("KEYSTORE_ALIAS"));
+        };
+    }
 }
