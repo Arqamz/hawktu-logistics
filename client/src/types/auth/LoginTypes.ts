d1a7c2fa432d2d5644851df57a57b4860bc6e7e9
Arqam Zia <arqam.mzia@gmail.com>
2024-10-18 18:58:45 +0500

    Setup axios, start login integration

---
+++ client/src/types/auth/LoginTypes.ts
@@ -0,0 +1,12 @@
+export interface LoginRequest {
+    email: string;
+    password: string;
+}
+  
+export interface LoginResponse {
+    token: string;
+    user: {
+        email: string;
+        name: string;
+    };
+}
