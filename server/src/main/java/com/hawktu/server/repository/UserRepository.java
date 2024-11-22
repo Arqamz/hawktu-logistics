7e7883fc39d68932e719b05e5138178bad1427c5
mdanishharoon <i220955@nu.edu.pk>
2024-11-23 01:25:00 +0500

    User functionality, added server/src/main/java/com/hawktu/server/repository/UserRepository.java

--- server/src/main/java/com/hawktu/server/repository/UserRepository.java
+++ server/src/main/java/com/hawktu/server/repository/UserRepository.java
@@ -2,8 +2,15 @@ package com.hawktu.server.repository;
 
 import com.hawktu.server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    // Custom query methods
+    Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
