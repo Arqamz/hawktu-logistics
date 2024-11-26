fc82d14085266e905f533a5e1fb60cb8b9454e64
Arqam Zia <arqam.mzia@gmail.com>
2024-11-26 14:52:17 +0500

    Finalising (hopefully) Seller

--- server/src/main/java/com/hawktu/server/models/Seller.java
+++ server/src/main/java/com/hawktu/server/models/Seller.java
@@ -1,5 +1,24 @@
 package com.hawktu.server.models;
 
-public class Seller {
-    
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
+public class Seller extends User {
+
+    @Column(nullable = false)
+    private String businessName;
+
+    public Seller(String password, String email, String firstName, String lastName, String phoneNumber, LocalDateTime registeredAt, Double wallet, String businessName) {
+        super(password, email, firstName, lastName, phoneNumber, registeredAt, wallet);
+        this.businessName = businessName;
+    }
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
 }
