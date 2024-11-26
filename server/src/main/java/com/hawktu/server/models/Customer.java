ef1e8fa8cc7f4d75d80850193a1d8aac7c0371c5
Arqam Zia <arqam.mzia@gmail.com>
2024-11-26 14:52:16 +0500

    Finalising (hopefully) Customer

--- server/src/main/java/com/hawktu/server/models/Customer.java
+++ server/src/main/java/com/hawktu/server/models/Customer.java
@@ -1,153 +1,23 @@
 package com.hawktu.server.models;
 
 import java.time.LocalDateTime;
+import jakarta.persistence.*;
 
-import com.hawktu.server.interfaces.ICustomer;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
-public class Customer implements ICustomer {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
-
-    @Column(nullable = false, unique = true)
-    private String email;
-
-    @Column(nullable = false)
-    private String firstName;
-
-    @Column(nullable = false)
-    private String lastName;
-
-    @Column(nullable = false)
-    private String phoneNumber;
-
-    @Column(nullable = false)
-    private LocalDateTime registeredAt;
-
-    @Column(nullable = false)
-    private Double walletBalance = 0.0;
+@Entity
+public class Customer extends User {
 
     @Column(nullable = false)
     private Integer loyaltyPoints = 0;
 
-    public Customer() {
-        // Default constructor for JPA
-    }
-
-    public Customer(Long id, String username, String password, String email, 
-                    String firstName, String lastName, String phoneNumber, 
-                    LocalDateTime registeredAt, Double walletBalance, Integer loyaltyPoints) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.phoneNumber = phoneNumber;
-        this.registeredAt = registeredAt;
-        this.walletBalance = walletBalance;
+    public Customer(String password, String email, String firstName, String lastName, String phoneNumber, LocalDateTime registeredAt, Double wallet, Integer loyaltyPoints) {
+        super(password, email, firstName, lastName, phoneNumber, registeredAt, wallet);
         this.loyaltyPoints = loyaltyPoints;
     }
 
-    @Override
-    public Long getId() {
-        return id;
-    }
-
-    @Override
-    public String getUsername() {
-        return username;
-    }
-
-    @Override
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @Override
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String getEmail() {
-        return email;
-    }
-
-    @Override
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    @Override
-    public String getFirstName() {
-        return firstName;
-    }
-
-    @Override
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    @Override
-    public String getLastName() {
-        return lastName;
-    }
-
-    @Override
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    @Override
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    @Override
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    @Override
-    public LocalDateTime getRegisteredAt() {
-        return registeredAt;
-    }
-
-
-    @Override
-    public Double getWalletBalance() {
-        return walletBalance;
-    }
-
-    @Override
-    public void setWalletBalance(Double walletBalance) {
-        this.walletBalance = walletBalance;
-    }
-
-    @Override
     public Integer getLoyaltyPoints() {
         return loyaltyPoints;
     }
 
-    @Override
     public void setLoyaltyPoints(Integer loyaltyPoints) {
         this.loyaltyPoints = loyaltyPoints;
     }
