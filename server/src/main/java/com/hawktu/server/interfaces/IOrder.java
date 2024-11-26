0e20371cd8896254fa1e013b7de0cf7841c9f59b
Arqam Zia <arqam.mzia@gmail.com>
2024-11-26 14:53:50 +0500

    Remove User interfaces

--- server/src/main/java/com/hawktu/server/interfaces/IOrder.java
+++ server/src/main/java/com/hawktu/server/interfaces/IOrder.java
@@ -8,8 +8,6 @@ import com.hawktu.server.models.OrderItem;
 public interface IOrder {
 
     Long getId();
-    String getOrderCode();
-    void setOrderCode(String orderCode);
     List<OrderItem> getOrderItems();
     void addOrderItem(OrderItem item);
     void removeOrderItem(OrderItem item);
